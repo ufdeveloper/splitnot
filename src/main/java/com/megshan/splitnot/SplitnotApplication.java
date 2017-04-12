bee031f112cab9f6bb971951bc6c93b4a90efa67
Shantanu <shantanu.sardal@citrix.com>
2017-04-11 23:47:34 -0700

    added SplitnotApplication.java

---
+++ src/main/java/com/megshan/splitnot/SplitnotApplication.java
@@ -0,0 +1,13 @@
+package com.megshan.splitnot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitnotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitnotApplication.class, args);
+    }
+
+}
