2fd4d6a13be42198a89bb082879fac8037aa2645
Shantanu <shantanu.sardal@citrix.com>
2017-04-11 23:53:53 -0700

    added account controller

---
+++ src/main/java/com/megshan/splitnot/controller/AccountController.java
@@ -0,0 +1,16 @@
+package com.megshan.splitnot.controller;
+
+/**
+ * Created by shantanu on 4/11/17.
+ */
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for adding/deleting accounts with plaid.
+ */
+@RestController
+public class AccountController {
+    
+
+}
