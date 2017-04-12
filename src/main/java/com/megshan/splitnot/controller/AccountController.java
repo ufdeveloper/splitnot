abc575a0a5dc1ddd986c3f23f17cc83ba81dcbfb
Shantanu <shantanu.sardal@citrix.com>
2017-04-11 23:58:11 -0700

    added lombok plugin

--- src/main/java/com/megshan/splitnot/controller/AccountController.java
+++ src/main/java/com/megshan/splitnot/controller/AccountController.java
@@ -4,13 +4,18 @@ package com.megshan.splitnot.controller;
  * Created by shantanu on 4/11/17.
  */
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Controller for adding/deleting accounts with plaid.
  */
+
+@Slf4j
 @RestController
 public class AccountController {
+
     
 
 }
