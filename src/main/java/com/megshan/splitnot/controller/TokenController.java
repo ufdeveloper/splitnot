4e241355a52a28e5063873b4a0c829927c128bed
Shantanu <shantanu.sardal@citrix.com>
2017-04-18 02:24:31 -0700

    remove unused class

--- src/main/java/com/megshan/splitnot/controller/TokenController.java
+++ src/main/java/com/megshan/splitnot/controller/TokenController.java
@@ -1,12 +1,10 @@
 package com.megshan.splitnot.controller;
 
-import com.megshan.splitnot.dto.token.PublicTokenRequest;
 import com.megshan.splitnot.service.TokenService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
